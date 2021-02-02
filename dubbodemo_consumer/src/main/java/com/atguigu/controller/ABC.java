eb4a7d117ccb67f209145d89e0d1199e4c796ec8
董翔琦 <752477768@qq.com>
2021-02-02 22:25:22 +0800

    修改了ABC类

--- dubbodemo_consumer/src/main/java/com/atguigu/controller/ABC.java
+++ dubbodemo_consumer/src/main/java/com/atguigu/controller/ABC.java
@@ -2,6 +2,6 @@ package com.atguigu.controller;
 
 public class ABC {
     public static void main(String[] args) {
-        System.out.println(1111111111);
+        System.out.println("111111111111111122222");
     }
 }
